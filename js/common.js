b91a1241a4fbef71a7abae0d8f5e4c60cf1d038e
tae126 <122263047+tae126@users.noreply.github.com>
2023-02-01 14:41:57 +0900

    portfolio 2

--- js/common.js
+++ js/common.js
@@ -1,15 +1,27 @@
 
-let header =document.querySelector('#header')
-let open =document.querySelector('#header .open')
-let close =document.querySelector('#header #nav .close')
+// let header =document.querySelector('#header')
+// let open =document.querySelector('#header .open')
+// let close =document.querySelector('#header #nav .close')
+// 
+// open.onclick = function() {
+//     header.classList.add('on')
+// }
+// close.onclick = function() {
+//     header.classList.remove('on')
+// }
 
-open.onclick = function() {
-    header.classList.add('on')
-}
-close.onclick = function() {
-    header.classList.remove('on')
-}
+$('#header .open').on('click', function() {
+    $(this).parent().addClass('on')
+})
+
+$('#header .close').on('click', function() {
+    $(this).parents('#header').removeClass('on')
+})
 
 $('#nav .depth1 > li > a').on('click', function() {
     $(this).parent().toggleClass('on')
+    $(this).parent().siblings().removeClass('on')
+    
 })
+
+
